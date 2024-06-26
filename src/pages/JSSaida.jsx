bc59af9b13243443230dc80f788eabbdb6a2020c
Eduardo fernandes da silva <eduardo.fernandes.silva13@gmail.com>
2024-06-26 15:58:45 -0300

    Atualizar estilos de layout e espaçamento nos componentes

--- src/pages/JSSaida.jsx
+++ src/pages/JSSaida.jsx
@@ -1,6 +1,5 @@
 import Codigo from "../components/Codigo";
 import Nota from "../components/Nota";
-import InfoBox from "../components/InfoBox";
 import InlineCode from "../components/InlineCode";
 import TextoComponent from "../components/TextComponent";
 import Separador from "../components/Separador";
@@ -228,9 +227,71 @@ function JSSaida() {
                     <br />
                     <br />
                     <code className="text-blue-600">&lt;</code>
+                    <code className="text-red-700">button</code>{" "}
+                    <code className="text-red-500">type</code>
+                    <code className="text-blue-600">=&quot;button&quot;</code>
+                    <code className="text-red-500"> onclick</code>
+                    <code className="text-blue-600">
+                        =&quot;document.write(5 + 6)&quot;
+                    </code>
+                    <code className="text-blue-600">&gt;</code>
+                    Tente
+                    <code className="text-blue-600">&lt;</code>
+                    <code className="text-red-700">/button</code>
+                    <code className="text-blue-600">&gt;</code>
+                    <br />
+                    <br />
+                    <code className="text-blue-600">&lt;</code>
+                    <code className="text-red-700">/body</code>
+                    <code className="text-blue-600">&gt;</code>
+                    <br />
+                    <code className="text-blue-600">&lt;</code>
+                    <code className="text-red-700">/html</code>
+                    <code className="text-blue-600">&gt;</code>
+                </Codigo>
+                <Nota>
+                    O método document.write() deve ser usado apenas para testes.
+                </Nota>
+            </TextoComponent>
+            <Separador />
+            <TextoComponent titulo="Usando window.alert()">
+                <p>Você pode usar uma caixa de alerta para mostrar dados:</p>
+                <Codigo titulo="Exemplo">
+                    <code className="text-blue-600">&lt;</code>
+                    <code className="text-red-700">!DOCTYPE </code>
+                    <code className="text-red-500">html</code>
+                    <code className="text-blue-600">&gt;</code>
+                    <br />
+                    <code className="text-blue-600">&lt;</code>
+                    <code className="text-red-700">html</code>
+                    <code className="text-blue-600">&gt;</code>
+                    <br />
+                    <code className="text-blue-600">&lt;</code>
+                    <code className="text-red-700">body</code>
+                    <code className="text-blue-600">&gt;</code>
+                    <br />
+                    <br />
+                    <code className="text-blue-600">&lt;</code>
+                    <code className="text-red-700">h1</code>
+                    <code className="text-blue-600">&gt;</code>
+                    Minha Primeira Página Web
+                    <code className="text-blue-600">&lt;</code>
+                    <code className="text-red-700">/h1</code>
+                    <code className="text-blue-600">&gt;</code>
+                    <br />
+                    <code className="text-blue-600">&lt;</code>
+                    <code className="text-red-700">p</code>
+                    <code className="text-blue-600">&gt;</code>Meu Primeiro
+                    Parágrafo
+                    <code className="text-blue-600">&lt;</code>
+                    <code className="text-red-700">/p</code>
+                    <code className="text-blue-600">&gt;</code>
+                    <br />
+                    <br />
+                    <code className="text-blue-600">&lt;</code>
                     <code className="text-red-700">script</code>
                     <code className="text-blue-600">&gt;</code> <br />
-                    document.write(
+                    window.alert(
                     <code className="text-red-500">5</code> +{" "}
                     <code className="text-red-500">6</code>);
                     <br />
@@ -247,6 +308,180 @@ function JSSaida() {
                     <code className="text-red-700">/html</code>
                     <code className="text-blue-600">&gt;</code>
                 </Codigo>
+                <p>
+                    Você pode pular a palavra-chave{" "}
+                    <InlineCode content="window" />.
+                </p>
+                <p>
+                    Em JavaScript, o objeto window é o objeto de escopo global.
+                    Isso significa que variáveis, propriedades e métodos, por
+                    padrão, pertencem ao objeto janela. Isso também significa
+                    que especificar a palavra-chave{" "}
+                    <InlineCode content="window" /> é opcional:
+                </p>
+                <Codigo titulo="Exemplo">
+                    <code className="text-blue-600">&lt;</code>
+                    <code className="text-red-700">!DOCTYPE </code>
+                    <code className="text-red-500">html</code>
+                    <code className="text-blue-600">&gt;</code>
+                    <br />
+                    <code className="text-blue-600">&lt;</code>
+                    <code className="text-red-700">html</code>
+                    <code className="text-blue-600">&gt;</code>
+                    <br />
+                    <code className="text-blue-600">&lt;</code>
+                    <code className="text-red-700">body</code>
+                    <code className="text-blue-600">&gt;</code>
+                    <br />
+                    <br />
+                    <code className="text-blue-600">&lt;</code>
+                    <code className="text-red-700">h1</code>
+                    <code className="text-blue-600">&gt;</code>
+                    Minha Primeira Página Web
+                    <code className="text-blue-600">&lt;</code>
+                    <code className="text-red-700">/h1</code>
+                    <code className="text-blue-600">&gt;</code>
+                    <br />
+                    <code className="text-blue-600">&lt;</code>
+                    <code className="text-red-700">p</code>
+                    <code className="text-blue-600">&gt;</code>Meu Primeiro
+                    Parágrafo
+                    <code className="text-blue-600">&lt;</code>
+                    <code className="text-red-700">/p</code>
+                    <code className="text-blue-600">&gt;</code>
+                    <br />
+                    <br />
+                    <code className="text-blue-600">&lt;</code>
+                    <code className="text-red-700">script</code>
+                    <code className="text-blue-600">&gt;</code> <br />
+                    alert(
+                    <code className="text-red-500">5</code> +{" "}
+                    <code className="text-red-500">6</code>);
+                    <br />
+                    <code className="text-blue-600">&lt;</code>
+                    <code className="text-red-700">/script</code>
+                    <code className="text-blue-600">&gt;</code>
+                    <br />
+                    <br />
+                    <code className="text-blue-600">&lt;</code>
+                    <code className="text-red-700">/body</code>
+                    <code className="text-blue-600">&gt;</code>
+                    <br />
+                    <code className="text-blue-600">&lt;</code>
+                    <code className="text-red-700">/html</code>
+                    <code className="text-blue-600">&gt;</code>
+                </Codigo>
+            </TextoComponent>
+            <Separador />
+            <TextoComponent titulo="Usando console.log()">
+                <p>
+                    Para fins de depuração, você pode chamar o método{" "}
+                    <InlineCode content="console.log()" /> no navegador para
+                    exibir dados.
+                </p>
+                <Nota>
+                    Você irá aprender mais sobre depuração em um capítulo
+                    posterior.
+                </Nota>
+                <Codigo titulo="Exemplo">
+                    <code className="text-blue-600">&lt;</code>
+                    <code className="text-red-700">!DOCTYPE </code>
+                    <code className="text-red-500">html</code>
+                    <code className="text-blue-600">&gt;</code>
+                    <br />
+                    <code className="text-blue-600">&lt;</code>
+                    <code className="text-red-700">html</code>
+                    <code className="text-blue-600">&gt;</code>
+                    <br />
+                    <code className="text-blue-600">&lt;</code>
+                    <code className="text-red-700">body</code>
+                    <code className="text-blue-600">&gt;</code>
+                    <br />
+                    <br />
+                    <code className="text-blue-600">&lt;</code>
+                    <code className="text-red-700">script</code>
+                    <code className="text-blue-600">&gt;</code> <br />
+                    console.log(
+                    <code className="text-red-500">5</code> +{" "}
+                    <code className="text-red-500">6</code>);
+                    <br />
+                    <code className="text-blue-600">&lt;</code>
+                    <code className="text-red-700">/script</code>
+                    <code className="text-blue-600">&gt;</code>
+                    <br />
+                    <br />
+                    <code className="text-blue-600">&lt;</code>
+                    <code className="text-red-700">/body</code>
+                    <code className="text-blue-600">&gt;</code>
+                    <br />
+                    <code className="text-blue-600">&lt;</code>
+                    <code className="text-red-700">/html</code>
+                    <code className="text-blue-600">&gt;</code>
+                </Codigo>
+            </TextoComponent>
+            <Separador />
+            <TextoComponent titulo="JavaScript Print">
+                <p>JavaScript não possui nenhum método ou objeto print.</p>
+                <p>
+                    Você não pode acessar dispositivos de saída no JavaScript.
+                </p>
+                <p>
+                    A única excessão é que você pode chamar o método{" "}
+                    <InlineCode content="window.print()" /> no navegador para
+                    imprimir o conteúdo da janela atual.
+                </p>
+                <Codigo titulo="Exemplo">
+                    <code className="text-blue-600">&lt;</code>
+                    <code className="text-red-700">!DOCTYPE </code>
+                    <code className="text-red-500">html</code>
+                    <code className="text-blue-600">&gt;</code>
+                    <br />
+                    <code className="text-blue-600">&lt;</code>
+                    <code className="text-red-700">html</code>
+                    <code className="text-blue-600">&gt;</code>
+                    <br />
+                    <code className="text-blue-600">&lt;</code>
+                    <code className="text-red-700">body</code>
+                    <code className="text-blue-600">&gt;</code>
+                    <br />
+                    <br />
+                    <code className="text-blue-600">&lt;</code>
+                    <code className="text-red-700">button</code>
+                    <code className="text-red-500"> onclick</code>
+                    <code className="text-blue-600">
+                        =&quot;window.print()&quot;
+                    </code>
+                    <code className="text-blue-600">&gt;</code>
+                    Imprimir esta página
+                    <code className="text-blue-600">&lt;</code>
+                    <code className="text-red-700">/button</code>
+                    <code className="text-blue-600">&gt;</code>
+                    <br />
+                    <br />
+                    <code className="text-blue-600">&lt;</code>
+                    <code className="text-red-700">/body</code>
+                    <code className="text-blue-600">&gt;</code>
+                    <br />
+                    <code className="text-blue-600">&lt;</code>
+                    <code className="text-red-700">/html</code>
+                    <code className="text-blue-600">&gt;</code>
+                </Codigo>
+            </TextoComponent>
+            <TextoComponent>
+                <div className="flex justify-between">
+                    <Link
+                        to="/js-onde"
+                        className="p-2 text-lg antialiased font-semibold text-white bg-green-600 rounded hover:bg-green-700 w-fit"
+                    >
+                        Anterior
+                    </Link>
+                    <Link
+                        to="/js-declaracoes"
+                        className="p-2 text-lg antialiased font-semibold text-white bg-green-600 rounded hover:bg-green-700 w-fit"
+                    >
+                        Próximo
+                    </Link>
+                </div>
             </TextoComponent>
         </div>
     );
