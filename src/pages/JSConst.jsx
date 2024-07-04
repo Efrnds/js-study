1013de1f43e8c9691120c7c6b222780eaffe2308
efrnds <edudfrs@gmail.com>
2024-07-04 12:25:40 -0300

    asd

--- src/pages/JSConst.jsx
+++ src/pages/JSConst.jsx
@@ -1,6 +1,7 @@
 import Codigo from "../components/Codigo";
 import Separador from "../components/Separador";
 import TextoComponent from "../components/TextComponent";
+import Nota from "../components/Nota";
 import InlineCode from "../components/InlineCode";
 import InfoBox from "../components/InfoBox";
 import { Link } from "react-router-dom";
@@ -71,7 +72,42 @@ function JSConst() {
             <Separador />
             <TextoComponent titulo="Deve ser atribuído">
                 <p>Variáveis <InlineCode content="const"/> do JavaScript devem ser atribuídas a um valor quando são declaradas:</p>
+            <Codigo titulo="Correto">
+                <code className="text-blue-500">const</code> PI = <code className="text-red-500">3.14159265359</code>;
+            </Codigo>
+            <Codigo titulo="Incorreto" bg={"red-200"}>
+            <code className="text-blue-500">const</code> PI;<br />
+            PI = <code className="text-red-500">3.14159265359</code>;
+            </Codigo>
+            <Nota titulo="Quando usar a const no JavaScript">
+                <p><b>Sempre declare uma variável com <InlineCode content="const"/> quando você sabe que o valor da variável não deve ser alterado.</b></p>
+                <p>Use <InlineCode content="const"/> quando você for declarar:</p>
+                <ul className="pl-10 list-disc">
+                    <li>Um novo Array.</li>
+                    <li>Um novo Objeto.</li>
+                    <li>Uma nova Função.</li>
+                    <li>Um novo RegExp.</li>
+                </ul>
+            </Nota>
             </TextoComponent>
+            <Separador />
+            <TextoComponent titulo="Objetos Constantes e Arrays">
+                <p>A palavra-chave <InlineCode content="const"/> é um pouco confusa.</p>
+                <p>Ela não define um valor constante. Ela define uma referência constante à um valor.</p>
+                <p>Por conta disso você NÃO pode:</p>
+                <ul className="pl-10 list-disc">
+                    <li>Reatribuir um valor constante.</li>
+                    <li>Reatribuir um array constante.</li>
+                    <li>Reatribuir um objeto constante.</li>
+                </ul>
+                <p>Mas você PODE:</p>
+                <ul className="pl-10 list-disc">
+                    <li>Alterar os elementos de um array constante.</li>
+                    <li>Alterar as propriedades de um objeto constante.</li>
+                </ul>
+            </TextoComponent>
+            <Separador />
+            <TextoComponent titulo="Arrays Constantes"></TextoComponent>
         </div>
     );
 }
